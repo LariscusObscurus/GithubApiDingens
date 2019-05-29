c91dc09f001ead028d1efb52f0d5f40470745d52
leonhardt <lariscusObscurus@gmail.com>
2019-05-30 00:17:02 +0200

    initial commit

---
+++ GithubApiDingens/Program.cs
@@ -0,0 +1,26 @@
+﻿using Octokit;
+using System;
+using System.Linq;
+
+namespace GithubApiDingens
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("GithubApiDingens"));
+            var basicAuth = new Credentials("<UserName>", "<Password>");
+            
+            client.Credentials = basicAuth;
+            var myRepos = await client.Repository.GetAllForCurrent();
+            foreach (var repository in myRepos)
+            {
+                var request = new RepositoryTrafficRequest(TrafficDayOrWeek.Week);
+                var views = await client.Repository.Traffic.GetViews(repository.Id, request);
+                var clones = await client.Repository.Traffic.GetClones(repository.Id, request);
+                Console.WriteLine($"{repository.Owner.Login}/{repository.Name} has {views.Count} views and {clones.Count} clones.");
+            }
+
+        }
+    }
+}
